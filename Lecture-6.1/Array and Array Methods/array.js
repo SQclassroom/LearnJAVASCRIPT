fc8ebf07f528dcab18ec9cb3e12d41341bf43229
SQclassroom <sq.vivek@gmail.com>
2023-06-16 11:49:37 +0530

    javascript16-06

---
+++ Lecture-6.1/Array and Array Methods/array.js
@@ -0,0 +1,383 @@
+// Javascript Array
+
+// const fruits = [];
+// fruits.push("banana", "apple", "peach");
+// console.log(fruits.length);
+// fruits[5] = "mango";
+// console.log(fruits[5]);
+// console.log(Object.keys(fruits));
+// console.log(fruits.length);
+// fruits.length = 10;
+// console.log(fruits);
+// console.log(Object.keys(fruits));
+// console.log(fruits.length);
+// console.log(fruits[8]);
+// fruits.length = 2;
+// console.log(Object.keys(fruits));
+// console.log(fruits.length);
+
+
+
+// array length properties
+// Accessing the First Array Element
+// Accessing the Last Array Element
+// Adding Array Elements
+// Converting Arrays to Strings
+
+/*
+
+Array indexes start with 0:
+
+[0] is the first array element
+[1] is the second 
+[2] is the third ...
+
+*/
+
+// let newarray = ["madhu" , "vishal" , "kishor" , "chirag"];
+// console.log(newarray[3] , newarray[3]);
+
+// {
+//     const newarrrays = [
+//         "one" ,
+//          "two" ,
+//           "three" ,
+//            "four"];  // javascript arrays
+    
+//     newarrrays[1] = "five"; // changing arrays element
+    
+//     console.log(newarrrays);
+// }
+
+// array length properties
+
+// {
+//     const newarrrays = [
+//         "one" ,
+//          "two" ,
+//           "three" ,];  // javascript arrays
+          
+//     console.log(newarrrays.length); // array length
+// }
+
+
+// Accessing the First Array Element
+
+// {
+//     const fruits = ["Banana", "Orange", "Apple", "Mango"];
+//     console.log(fruits[0]);
+// }
+
+// Accessing the Last Array Element
+
+// {
+//     var fruits = ["Banana", "Orange", "Apple", "Mango"];
+//     document.getElementById("demo").innerHTML = fruits[fruits.length-3];
+// }
+
+// Adding Array Elements
+
+// {
+//     const fruits = ["Banana", "Orange", "Apple"];
+//     document.getElementById("demo").innerHTML = fruits;
+    
+//     function myFunction() {
+//         fruits.push("Lemon");
+//         document.getElementById("demo-1").innerHTML = fruits;
+//     }
+// }
+
+// The length property provides an easy way to append new elements to an array without using the push() method.
+
+// {
+//     const fruits = ["Banana", "Orange", "Apple"];
+//     document.getElementById("demo").innerHTML = fruits;
+
+//     function myFunctionn() {
+//     fruits[fruits.length] = "Lemon";
+//     document.getElementById("demo-1").innerHTML = fruits;
+//     }
+// }
+
+// Adding elements with high indexes can create undefined "holes" in an array.
+
+// {
+//     const fruits = ["Banana", "Orange", "Apple"];
+//     fruits[6] = "Lemon";
+//     console.log(fruits);
+
+//     let fLen = fruits.length;
+//     let text = "";
+//     for (i = 0; i < fLen; i++) {
+//     text += fruits[i] + "<br>";
+//     }
+
+//     document.getElementById("demo").innerHTML = text;
+// }
+
+// Array Methods
+
+// Converting Arrays to Strings
+
+// {
+//     const fruits = ["Banana", "Orange", "Apple"];
+//     console.log(fruits.toString());
+// }
+
+// The join() method also joins all array elements into a string.
+
+// {
+//     const fruits = ["Banana", "Orange", "Apple", "Mango"];
+//     document.getElementById("demo").innerHTML = fruits.join(" ' ");
+// }
+
+// Popping and Pushing
+
+// {
+//     const fruits = ["Banana", "Orange", "Apple", "Mango"];
+//     document.getElementById("demo").innerHTML = fruits;
+//     fruits.pop();
+//     fruits.pop();
+//     fruits.pop();
+//     document.getElementById("demo-1").innerHTML = fruits;
+// }
+
+// The pop() method returns the value that was "popped out"
+
+// {
+//     const fruits = ["Banana", "Orange", "Apple", "Mango"];
+//     document.getElementById("demo").innerHTML = fruits.pop();
+//     document.getElementById("demo-1").innerHTML = fruits;
+// }
+
+// JavaScript Array push()
+//The push() method adds a new element to an array (at the end)
+
+// {
+//     const fruits = ["Banana", "Orange", "Apple", "Mango"];
+//     document.getElementById("demo").innerHTML = fruits;
+//     fruits.push("Kiwi");
+//     document.getElementById("demo-1").innerHTML = fruits;
+// }
+
+// The push() method returns the new array length
+
+// {
+//     const fruits = ["Banana", "Orange", "Apple", "Mango"];
+//     document.getElementById("demo").innerHTML = fruits.push("Kiwi");
+//     document.getElementById("demo-1").innerHTML = fruits.length;
+// }
+
+//Shifting Elements
+//JavaScript Array shift()
+
+//The shift() method removes the first array element and "shifts" all other elements to a lower index
+
+// {
+//     const fruits = ["Banana", "Orange", "Apple", "Mango"];
+//     document.getElementById("demo").innerHTML = fruits;
+//     fruits.shift();
+//     fruits.shift();
+//     document.getElementById("demo-1").innerHTML = fruits;
+// }
+
+// The shift() method returns the value that was "shifted out"
+
+// {
+//     const fruits = ["Banana", "Orange", "Apple", "Mango"];
+//     document.getElementById("demo").innerHTML = fruits.shift();
+//     document.getElementById("demo-1").innerHTML = fruits;
+// }
+
+// JavaScript Array unshift()
+//The unshift() method adds a new element to an array (at the beginning), and "unshifts" older elements
+
+// {
+//     const fruits = ["Banana", "Orange", "Apple", "Mango"];
+//     document.getElementById("demo").innerHTML = fruits;
+//     fruits.unshift("Lemon");
+//     document.getElementById("demo-1").innerHTML = fruits;
+// }
+
+// The unshift() method returns the new array length
+
+// {
+//     const fruits = ["Banana", "Orange", "Apple", "Mango"];
+//     document.getElementById("demo").innerHTML = fruits.unshift("Lemon");
+//     document.getElementById("demo-1").innerHTML = fruits;
+// }
+
+//  Changing Elements
+
+// {
+//     const fruits = ["Banana", "Orange", "Apple", "Mango"];
+//     document.getElementById("demo").innerHTML = fruits;
+//     fruits[1] = "Kiwi";
+//     document.getElementById("demo-1").innerHTML = fruits;
+// }
+
+//JavaScript Array add element
+
+// {
+//     const fruits = ["Banana", "Orange", "Apple", "Mango"];
+//     document.getElementById("demo").innerHTML = fruits;
+//     fruits[fruits.length] = "Kiwi";
+//     document.getElementById("demo-1").innerHTML = fruits;
+// }
+
+//JavaScript Array delete()
+
+//Array elements can be deleted using the JavaScript operator delete.
+
+//Using delete leaves undefined holes in the array.
+
+//Use pop() or shift() instead.
+
+{
+    // const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+    // document.getElementById("demo").innerHTML =fruits;
+
+    // delete fruits[2];
+
+    // fruits[2] = "kiwi";
+
+    // document.getElementById("demo-1").innerHTML =fruits;
+}
+
+//Merging (Concatenating) Arrays
+
+// {
+//     const myGirls = ["Cecilie", "Lone"];
+//     const myboys1 = ["Emil", "Tobias", "Linus"];
+//     const myboys2 = ["ajay" ,"vijay" ,"sunjay"];
+//     const myChildren = myGirls.concat(myboys1 , myboys2);
+
+//     document.getElementById("demo").innerHTML = myChildren;
+//     console.log(myChildren);
+// }
+
+//The concat() method does not change the existing arrays. It always returns a new array.
+
+//Splicing and Slicing Arrays
+
+//JavaScript Array splice()
+
+// {
+//     const fruits = ["Banana", "Orange", "Apple", "Mango"];
+//     document.getElementById("demo").innerHTML = fruits;
+
+//     fruits.splice(0 , 3, "Lemon", "Kiwi" , "Watermelon");
+//     document.getElementById("demo-1").innerHTML = fruits;
+// }
+
+// {
+//     const fruits = ["Banana", "Orange", "Apple", "Mango"];
+//     document.getElementById("demo").innerHTML = "Original Array:<br> " + fruits;
+//     let removed = fruits.splice(2, 2, "Lemon", "Kiwi"); 
+//     document.getElementById("demo-1").innerHTML = "New Array:<br>" + fruits;
+//     document.getElementById("demo-2").innerHTML = "Removed Items:<br> " + removed; 
+// }
+
+// Using splice() to Remove Elements
+
+// {
+//     const fruits = ["Banana", "Orange", "Apple", "Mango"];
+//     document.getElementById("demo-1").innerHTML = fruits;
+//     fruits.splice(0, 1);
+//     document.getElementById("demo-2").innerHTML = fruits;
+// }
+
+// JavaScript Array slice()
+
+// {
+//     const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+//     const newremove = fruits.slice(1);
+//     document.getElementById("demo").innerHTML = fruits + "<br><br>" + newremove;
+// }
+
+// Array.isArray
+
+// {
+//     let book = ["math"  , "chemistry"  , "physics" , "bio"];
+//     let books = "string";
+//     console.log(Array.isArray(book));
+// }
+
+// javascript Sorting Array
+
+// {
+//     let book = ["math"  , "chemistry"  , "physics" , "bio" , "amazon"];
+//     let books = book.sort();
+//     console.log(books);
+// }
+
+// Reversing array
+
+// {
+//     let book = ["math"  , "chemistry"  , "physics" , "bio"];
+//     console.log(book);
+//     let bookss = book.reverse();
+//     console.log(bookss);
+//     let books =book.sort();
+//     console.log(books);
+// }
+
+// Number Sort
+
+// {
+//     const number1 = [40, 12, 15, 81 ,150 ,200];
+//     console.log(number1);
+//     const number2 = number1.sort(function(a , b){return a - b});
+//     console.log(number2);
+// }
+
+// Javascript Array Iteration
+
+// {
+//     const number1 = [45 , 56 , 78 , 23 , 96];
+//     let number2 = "";
+//     number1.forEach(myFunction);
+
+//     document.write(number2);
+//     function myFunction(value, index, array){
+//         number2 += value * 2 + "<br>";
+//     }
+
+// }
+
+
+// javascript array map()
+
+// {
+//     const number1 = [56 , 798 , 345  ,35];
+//     const number2 = number1.map(myFunction);
+
+//     console.log(number2);
+//     function myFunction(value , index  , array){
+//         return value * 2;
+//     }
+// }
+
+
+// javascript Array flatMap()
+
+// {
+//     const myarr = [1, 2, 3, 4, 5, 6];
+//     const newarr = myarr.flatMap((x) => x*2);
+//     console.log(newarr);
+// }
+
+// javascript array filter()
+
+// {
+//     const number1  =[45, 17 , 2, 46 , 78 , 15];
+//     const number2 = number1.filter(myFunction);
+
+//     function myFunction(age){
+//         return age <= 100;
+//     }
+//     console.log(number2);
+// }
+
+
---
+++ Lecture-6.2/Math Object/Math.js
@@ -0,0 +1,168 @@
+/*
+
+Maths Object
+The JavaScript Math object allows you to perform mathematical tasks on numbers.
+
+Example:-
+Math.PI;	
+The Math object is static.
+All methods and properties can be used without creating a Math object first.
+Math Properties(Constants):-
+The syntax for any Math property is : Math.property
+JavaScript provides 8 mathematical constants that can be accessed as Math properties. 
+
+*/
+
+/*
+
+Example:-
+
+Math.E        // returns Euler's number
+Math.PI       // returns PI
+Math.SQRT2    // returns the square root of 2
+Math.SQRT1_2  // returns the square root of 1/2
+Math.LN2      // returns the natural logarithm of 2
+Math.LN10     // returns the natural logarithm of 10
+Math.LOG2E    // returns base 2 logarithm of E
+Math.LOG10E   // returns base 10 logarithm of E
+Math Methods
+The syntax for Math any methods is : Math.method(number)
+
+*/
+
+//Math.round():-
+//Math.round(x) returns the nearest integer.
+//Example:-
+
+		// let math1 = Math.round(4.5);		// 5
+		// console.log(math1);
+		// let math2 = Math.round(4.4);		// 4
+		// console.log(math2);
+
+//Math.ceil():-
+//Math.ceil(x) returns the value of x rounded up to its nearest integer.
+//Example:-
+
+		// let math3 = Math.ceil(4.9);		// 5
+		// console.log(math3);
+		// let math3_1 = Math.ceil(3.8);	
+		// console.log(math3_1)	         // 4
+		// let math3_2 = Math.ceil(-4.3);	// -4
+		// console.log(math3_2);
+
+//Math.floor():-
+//Math.floor(x) returns the value of x rounded down to its nearest integer.
+//Example:-
+
+	// let math4 =	Math.floor(4.9);		// 4
+	// console.log(math4);
+	// Math.floor(4.2);		// 4
+	// Math.floor(-4.3);		// -5
+
+//Math.trunc():-
+//Math.trunc(x) returns the integer part of x.
+//Example:-
+
+		// let math5 = Math.trunc(5.9);		// 5
+		// console.log(math5);
+		// Math.trunc(4.2);		// 4
+
+//Math.sign():-
+//Math.sign(x) returns  if x is negative, null or positive.
+//Example:-
+
+		// let math6 = Math.sign(15);		// 1
+		// console.log(math6);	
+		// let math6_1 = Math.sign(-180);
+		// console.log(math6_1);
+		// let math6_2 = Math.sign(0);
+		// console.log(math6_2);
+
+//Math.pow():-
+//Math.pow(x, y) returns the value of x to the power of y.
+//Example:-
+
+		// let math7 = Math.pow(5, 5);		// 243 //3x3x3x3x3
+		// console.log(math7);
+		// Math.pow(8, 10);		// 64
+
+//Math.sqrt():-
+//Math.sqrt(x) returns the square root of x.
+//Example:-
+
+		// let math8 = Math.sqrt(64);		// 8
+		// console.log(math8);
+		// Math.sqrt(121);		// 11
+
+//Math.abs():-
+//Math.abs(x) returns the absolute (positive) value of x.
+//Example:-
+
+		// let math9 = Math.abs(-6);		// 6
+		// console.log(math9);
+		// let math9_1 = Math.abs(1.2);		// 1.2
+		// console.log(math9_1);
+
+//Math.min() and Math.max():-
+//Math.min() and Math.max() can be used to find the lowest or highest value in a list of arguments.
+//Example:-
+
+		// let math10 = Math.min(0, 150, 30, 20, -8, -200 , -250);	// -250
+		// console.log(math10);
+		// let math11 = Math.max(0, 150, 30, 20, -8, -200 , 500);	// 500
+		// console.log(math11);
+
+//Math.random():-
+//Math.random() returns a random number between 0 (inclusive), and 1 (exclusive).
+//Example:-
+
+		// let  math12 = Math.random();
+		// console.log(math12);
+		// console.log(math12);
+		// console.log(math12);
+
+
+//Math.sin():-
+//Math.sin(x) returns the sine (a value between -1 and 1) of the angle x (given in radians).
+//If you want to use degrees instead of radians, you have to convert degrees to radians:
+//	Angle in radians = Angle in degrees x PI / 180.
+//Example:-
+
+		// let math13 = Math.sin(270 * Math.PI / 180); //  1 (the sine of 90 degrees)
+		// console.log(math13);
+		// Math.sin(30 * Math.PI / 180); //  0.5 (the sine of 30 degrees)
+
+//Math.cos():-
+//Math.cos(x) returns the cosine (a value between -1 and 1) of the angle x (given in radians).
+//If you want to use degrees instead of radians, you have to convert degrees to radians:
+//	Angle in radians = Angle in degrees x PI / 180.
+//Example:-
+
+		// let math13_1 = Math.cos(270 * Math.PI / 180); //  1 (the cos of 0 degrees)
+		// console.log(math13_1);
+		// Math.cos(60 * Math.PI / 180); //  0.5 (the cos of 60 degrees)
+
+
+//Math.log():-
+//Math.log(x) returns the natural logarithm of x.
+//The natural logarithm returns the time needed to reach a certain level of growth.
+//Example:-
+
+		// let math14 = Math.log(15);	//  0 
+		// console.log(math14);
+		// Math.log(2);	//  0.6931471805599453
+		// Math.log(10);	//  2.302585092994046
+
+//Math.log2():-
+//Math.log2(x) returns the base 2 logarithm of x.
+//Example:-
+
+		// let math15 = Math.log2(20);	
+		// console.log(math15);
+
+//Math.log10():-
+//Math.log10(x) returns the base 10 logarithm of x.
+//Example:-
+
+		// let math16 = Math.log10(20);
+		// console.log(math16);
